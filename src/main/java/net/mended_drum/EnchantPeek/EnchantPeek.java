f5ea880a1f987d838a0ced7bdb9d0571bbac1c7b
Alexander Duscheleit <alexander.duscheleit@gmail.com>
2013-07-24 15:38:11 +0200

    Update imports for CraftBukkit 1.6.2 SNAPSHOT compatibility.

--- src/main/java/net/mended_drum/EnchantPeek/EnchantPeek.java
+++ src/main/java/net/mended_drum/EnchantPeek/EnchantPeek.java
@@ -1,18 +1,5 @@
 package net.mended_drum.EnchantPeek;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import net.minecraft.server.v1_5_R2.BiomeMeta;
-import net.minecraft.server.v1_5_R2.EnumCreatureType;
-import net.minecraft.server.v1_5_R2.IChunkProvider;
-import net.minecraft.server.v1_5_R2.MathHelper;
-import net.minecraft.server.v1_5_R2.WorldServer;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.craftbukkit.v1_5_R2.CraftWorld;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EnchantPeek extends JavaPlugin {
